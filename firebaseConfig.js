3f3ee23640ba48bd9eb317663ee35e8e263dedaf
colinmarley <colinmarley94@gmail.com>
2024-11-13 21:52:31 -0800

    Updates configs

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+
+require('dotenv').config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAGe9rzKe3Cjtzl342yBtAfRFOfQ8_A_yE",
-  authDomain: "my-media-db.firebaseapp.com",
-  projectId: "my-media-db",
-  storageBucket: "my-media-db.appspot.com",
-  messagingSenderId: "286526198339",
-  appId: "1:286526198339:web:b8b0bf81e52881c0c53af6",
-  measurementId: "G-EWLZTJM3KB"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
