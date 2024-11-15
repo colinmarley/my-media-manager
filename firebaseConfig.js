9237cb3c850dd5634925e08d207adb6cf6dbac61
colinmarley <colinmarley94@gmail.com>
2024-11-14 18:07:17 -0800

    Adds firebase firestore service to manage reading and writing

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -11,7 +12,7 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
@@ -21,5 +22,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 export default app;
---
+++ src/service/FirestoreService.ts
@@ -0,0 +1,41 @@
+import { db } from '../../firebaseConfig';
+import { collection, addDoc, getDocs, query, where, DocumentData } from 'firebase/firestore';
+
+class FirestoreService {
+  private collectionName: string;
+
+  constructor(collectionName: string) {
+    this.collectionName = collectionName;
+  }
+
+  async addDocument(data: DocumentData): Promise<void> {
+    try {
+      const docRef = await addDoc(collection(db, this.collectionName), data);
+      console.log('Document written with ID: ', docRef.id);
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  }
+
+  async getDocuments(): Promise<DocumentData[]> {
+    const q = query(collection(db, this.collectionName));
+    const querySnapshot = await getDocs(q);
+    const documents: DocumentData[] = [];
+    querySnapshot.forEach((doc) => {
+      documents.push({ id: doc.id, ...doc.data() });
+    });
+    return documents;
+  }
+
+  async getDocumentsByField(field: string, value: any): Promise<DocumentData[]> {
+    const q = query(collection(db, this.collectionName), where(field, '==', value));
+    const querySnapshot = await getDocs(q);
+    const documents: DocumentData[] = [];
+    querySnapshot.forEach((doc) => {
+      documents.push({ id: doc.id, ...doc.data() });
+    });
+    return documents;
+  }
+}
+
+export default FirestoreService;
